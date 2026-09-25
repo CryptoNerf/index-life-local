8cf4ec0ef5b36338964d7cca341a2c6495f3d0da
Emile Alexanyan <nerf.file@gmail.com>
2026-09-25 18:18:39 +0400

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/index-life-local/sw.js', { scope: '/index-life-local/' })})}
